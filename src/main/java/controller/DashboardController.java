f76fecd1b3ea470b83ce9c4efc5e6df1fa693b45
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-04-01 01:45:38 +0530

    feat: add real-time clock and date display to dashboard

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -1,4 +1,42 @@
 package controller;
 
-public class DashboardController {
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        localTime();
+    }
+    private void localTime(){
+        Date date =new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        lblDate.setText(simpleDateFormat.format(date));
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.ZERO, actionEvent -> {
+            LocalTime now = LocalTime.now();
+            lblTime.setText(now.getHour() + ":" + now.getMinute() + ":" + now.getSecond());
+        }),
+                new KeyFrame(Duration.seconds(1))
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
 }
