089118e4273b433f9a6617c0192f49d1693b69a0
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-03-31 23:29:54 +0530

    feat: add DashboardFormController with button action methods

---
+++ src/main/java/controller/DashboardFormController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DashboardFormController {
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderDetailsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderOnAction(ActionEvent event) {
+
+    }
+
+}
