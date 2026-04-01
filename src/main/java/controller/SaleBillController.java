24510ff4cb08da6f4f88b19bee4c1c03d1db085a
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-04-01 22:27:07 +0530

    feat: refactor SaleBill form and controller for improved structure and readability

--- src/main/java/controller/SaleBillController.java
+++ src/main/java/controller/SaleBillController.java
@@ -1,38 +1,149 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import service.ServiceFactory;
+import service.custom.SaleBillService;
+import util.ServiceType;
 
-public class SaleBillController {
-    public void checkLowStock(ActionEvent actionEvent) {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SaleBillController implements Initializable {
+    SaleBillService saleBillService = ServiceFactory.getInstance().getServiceType(ServiceType.SALEBILL);
+
+
+    @FXML
+    private TableColumn<?, ?> actionColumn;
+
+    @FXML
+    private TableView<?> cartTable;
+
+    @FXML
+    private TextArea customerAddressField;
+
+    @FXML
+    private TableColumn<?, ?> customerColumn;
+
+    @FXML
+    private TextField customerContactField;
+
+    @FXML
+    private TextField customerNameField;
+
+    @FXML
+    private TableColumn<?, ?> dateColumn;
+
+    @FXML
+    private TableColumn<?, ?> invoiceIdColumn;
+
+    @FXML
+    private ComboBox<?> medicineComboBox;
+
+    @FXML
+    private TableColumn<?, ?> medicineNameColumn;
+
+    @FXML
+    private TableColumn<?, ?> qtyColumn;
+
+    @FXML
+    private TextField quantityField;
+
+    @FXML
+    private TableView<?> salesHistoryTable;
+
+    @FXML
+    private TextField searchHistoryField;
+
+    @FXML
+    private TableColumn<?, ?> statusColumn;
+
+    @FXML
+    private Label subtotalLabel;
+
+    @FXML
+    private Label taxLabel;
+
+    @FXML
+    private TableColumn<?, ?> totalAmountColumn;
+
+    @FXML
+    private TableColumn<?, ?> totalColumn;
+
+    @FXML
+    private Label totalLabel;
+
+    @FXML
+    private TableColumn<?, ?> unitPriceColumn;
+
+    @FXML
+    private TextField unitPriceField;
+
+    @FXML
+    void addToCart(ActionEvent event) {
+
     }
 
-    public void showDailyReport(ActionEvent actionEvent) {
+    @FXML
+    void checkLowStock(ActionEvent event) {
+
     }
 
-    public void addToCart(ActionEvent actionEvent) {
+    @FXML
+    void clearCart(ActionEvent event) {
+
     }
 
-    public void removeFromCart(ActionEvent actionEvent) {
+    @FXML
+    void generateInvoice(ActionEvent event) {
+
     }
 
-    public void clearCart(ActionEvent actionEvent) {
+    @FXML
+    void printInvoice(ActionEvent event) {
+
     }
 
-    public void generateInvoice(ActionEvent actionEvent) {
+    @FXML
+    void refreshSalesHistory(ActionEvent event) {
+
     }
 
-    public void printInvoice(ActionEvent actionEvent) {
+    @FXML
+    void removeFromCart(ActionEvent event) {
+
     }
 
-    public void searchSalesHistory(ActionEvent actionEvent) {
+    @FXML
+    void reprintInvoice(ActionEvent event) {
+
     }
 
-    public void refreshSalesHistory(ActionEvent actionEvent) {
+    @FXML
+    void searchSalesHistory(ActionEvent event) {
+
     }
 
-    public void viewInvoiceDetails(ActionEvent actionEvent) {
+    @FXML
+    void showDailyReport(ActionEvent event) {
+
     }
 
-    public void reprintInvoice(ActionEvent actionEvent) {
+    @FXML
+    void viewInvoiceDetails(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
+
